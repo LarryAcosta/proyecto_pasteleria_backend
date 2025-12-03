90399f7a4e19fa902e3dfc3ad0aeea787bb23c50
Larry Acosta <larryacostamendes@gmail.com>
2025-12-02 20:33:43 -0500

    Deploy listo para Render

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 from pydantic_settings import BaseSettings
 from functools import lru_cache
 
+
 class Settings(BaseSettings):
+    # Valores por defecto para que NO falle si no hay variables de entorno
     app_name: str = "Pasteleria API"
     app_env: str = "production"
     debug: bool = False
@@ -9,6 +11,8 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
+        env_file_encoding = "utf-8"
+
 
 @lru_cache
 def get_settings():
