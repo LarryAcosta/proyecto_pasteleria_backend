9909fb7eca4a7c3fe76d8a3a936175c3b4bc63bd
Larry Acosta <larryacostamendes@gmail.com>
2025-12-02 20:17:23 -0500

    Deploy listo para Render

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ from pydantic_settings import BaseSettings
 from functools import lru_cache
 
 class Settings(BaseSettings):
-    app_name: str
-    app_env: str
-    debug: bool
-    database_url: str
+    app_name: str = "Pasteleria API"
+    app_env: str = "production"
+    debug: bool = False
+    database_url: str = "sqlite:///./pasteleria.db"
 
     class Config:
         env_file = ".env"
